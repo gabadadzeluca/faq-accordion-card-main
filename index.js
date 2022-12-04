5c754461e0047783650d08af77fe2f95c6569326
Luca <gabadadzeluca@gmail.com>
2022-12-04 22:41:00 +0400

    write toggle logic

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
+const faq = document.querySelectorAll('.faq div');
+
 // add classes to all elements
 document.addEventListener('DOMContentLoaded', ()=>{
-    
-    const faq = document.querySelectorAll('.faq div');
     faq.forEach(element =>{
         addArrow(element);
         let question = element.firstElementChild;
         let answer = element.lastElementChild;
         question.classList.add('question');
         answer.classList.add('answer');
-        console.log(question , answer); 
-        // element.addEventListener('click', display);
+        element.addEventListener('click', display);
+        console.log(element);
     })  
 });
 
-
 // display the answer of a selected question
-function display(e){
-    const box = e.target;
-    console.log(box);
-    console.log("parent: ",box.parentElement);
-    box.parentElement.lastElementChild.style.display = 'block';
+function display(){
+    faq.forEach(element=>{
+        if(element != this){
+            element.lastElementChild.style.display = 'none';
+        }
+    })
+    let content = this.lastElementChild;
+    console.log(content);
+    if(content.style.display === 'block'){
+        content.style.display = 'none';
+    }else{
+        this.classList.toggle('open');
+        content.style.display = 'block';
+    }
 }
-
 
 // add arrow to each question
 function addArrow(element){
     const arrow = document.createElement('div');
-    arrow.classList.add("arrow-down");
+    arrow.classList.add('arrow-down');
     element.firstElementChild.appendChild(arrow);
 }
